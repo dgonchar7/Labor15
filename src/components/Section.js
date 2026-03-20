04c167db11c03f14de0d2d35c197b640d22ee2f3
dgonchar7 <dgonchar@sfedu.ru>
2026-03-20 05:37:10 +0300

    Section.js

---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+function Section() {
+  return (
+    <div className="section">
+      <img src="https://via.placeholder.com/150" alt="placeholder" />
+    </div>
+  );
+}
+
+export default Section;
