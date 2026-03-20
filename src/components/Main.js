ceae1e7941c6b6fe9d51debfe73f6fc21aba9d06
dgonchar7 <dgonchar@sfedu.ru>
2026-03-20 05:37:25 +0300

    Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import Section from "./Section";
+import Article from "./Article";
+import Aside from "./Aside";
+
+function Main() {
+  return (
+    <div className="main">
+      <Section />
+      <Article />
+      <Aside />
+    </div>
+  );
+}
+
+export default Main;
